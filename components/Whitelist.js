ebc8213ebe5f07ec8b52b4771c0e25a94cda39f4
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-08-08 00:19:26 -0500

    Initial

---
+++ components/Whitelist.js
@@ -0,0 +1,108 @@
+export default function Whitelist4() {
+return {
+  '0x26043762cbaA719cE3194d659278467E29A9ad2E': {
+    q: 1,
+    whitelistClaimPass: {
+      r: '0xb79717706626ecac11f5ae77f469c41aa1847050787b41b4835c4bc9777517ec',
+      s: '0x50600ffcff7e841cc2f36642555a3ff306f939fb57bb9fe67aff39d3d17e6348',
+      v: 28
+    }
+  },
+  '0xD0f6ddbC9Fb8B8Bac9Fb89AF8B21e640a4B47e8e': {
+    q: 1,
+    whitelistClaimPass: {
+      r: '0xb87078098b0c3cb9b19a619730e867d4e51038093ec4b10b942dfa161fa6f30c',
+      s: '0x7d4ac124751a52564f93f79e21fbe14c03d4c8cca795841b99036b84f86990d7',
+      v: 28
+    }
+  },
+  '0x3AC4161Dc070da4c6734c75538bb6AC64a0F0E63': {
+    q: 1,
+    whitelistClaimPass: {
+      r: '0x8320f4039d5b4365ec77bc6b7dfc6b0f067d304fa7af4aded2e0186edda6bb19',
+      s: '0x69bcf2dec41fe01febaa169b8c29c11a83dac6f31c835a1b7400baa1d964e6ca',
+      v: 28
+    }
+  },
+  '0x7d055494701e93173A766beB3Ba6Fd2f9b923d89': {
+    q: 7,
+    whitelistClaimPass: {
+      r: '0x0cb3beeb3cd9dd7ef49af04d7f474eef23ed32f7e1d2cc6cb52e018242df6be9',
+      s: '0x206d1ec13a77942a4fca691d38b442651b83ed46bc1cf97190f9b44feb234f9c',
+      v: 27
+    }
+  },
+  '0x55059722982A53b8FdC4EFF46cb5c2F6ef0aFB47': {
+    q: 6,
+    whitelistClaimPass: {
+      r: '0x4dbb41f5d645f67ac9bf21585750b93db7e6695bbed51096d220a083f42dc99d',
+      s: '0x528b234f016dbb3318e61d8bb67b7041841c4fb07b06231e6a029a482f9432e9',
+      v: 28
+    }
+  },
+  '0x4354A019e9e3471Ed86c86191428FD931992CfC8': {
+    q: 3,
+    whitelistClaimPass: {
+      r: '0x30bbfd372921c0e3e4d5f5eed488ab3106e7d33005abdfcdbad261e92b87032a',
+      s: '0x663e4afe591ca0adbd491925a527b37ed37335c22bc832ead76ab7f1055bcb24',
+      v: 27
+    }
+  },
+  '0xE3f9Cf2c9e51DD79a60FC78b20cA898dD01FcbfE': {
+    q: 1,
+    whitelistClaimPass: {
+      r: '0x453a391c57dde51ad56abdc83f0d93c77d2783cd8da3c908abbd08ba205807f8',
+      s: '0x18ee3f0edccda61750c20073938570695cff2523cf85cbfc9bc4f27a4406b566',
+      v: 27
+    }
+  },
+  '0xf8BE5B910c331f368fd724E5c76eE69ca892E6ae': {
+    q: 1,
+    whitelistClaimPass: {
+      r: '0xb912f7372beaca3e2e3bdced29ff6568b6cc05b854a6dabfbc193baba5f24525',
+      s: '0x29c22b1dad565f047b5234f4eac967411bb90523d83ab2d4c23cd5dafdae01de',
+      v: 28
+    }
+  },
+  '0xF9557eF7afb1A5b75C010A5303eC884443978Bf2': {
+    q: 1,
+    whitelistClaimPass: {
+      r: '0x76865ab1e6910059bf880a21cec4abea816b01e5808f0619c80fb47420188c8f',
+      s: '0x742cca668283bc23691dd01ddaaf191faa1bd3faee07d04d34a373c56a412104',
+      v: 27
+    }
+  },
+  '0xf00F459e6EeC67F3700316f6fef6da0DB8576aE3': {
+    q: 1,
+    whitelistClaimPass: {
+      r: '0x176a05ad60fef63b8235cadb97bf2d59a11534c105ac59a2d04ed0106f8bd7a2',
+      s: '0x2ceac0130ac46200af1fd9cb1f894d23b095f6e2787e962191c260d1beca52ec',
+      v: 27
+    }
+  },
+  '0xCd43AdcB61949ab14D3f4574BFbDA53d46389715': {
+    q: 4,
+    whitelistClaimPass: {
+      r: '0x945b395bc7e51ae494615e4ecf8ef8719da9172cee9bf789a3857c190467de3f',
+      s: '0x4092030c2144aeda6bfb2d03d877f9b3a24db1de26026d43861e8b7c8c34d33f',
+      v: 27
+    }
+  },
+  '0xA3F7d136d03a8F17032296ebc9d64928D120540A': {
+    q: 4,
+    whitelistClaimPass: {
+      r: '0xfda5fc24abfba4071b60b90c5951a958e6cb9bf3fba84082938024073e7af07a',
+      s: '0x466146bb5a0612c5f76cb79cf9842599b96f8d7f5b0df50694985fad275423cd',
+      v: 28
+    }
+  },
+  '0xa7531F5A9D56089A79EBCb295bAba41bED80ca22': {
+    q: 4,
+    whitelistClaimPass: {
+      r: '0xe50b57e87550f70e52b19a217188eeb38493fb2643c3365b45027c42af893724',
+      s: '0x119a9ec933af21b959b3e61ea1cd07698d9da794e9c852cac48ebe2b39f0b96d',
+      v: 27
+    }
+  }
+}
+}
---
+++ next.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    //reactStrictMode: false,
+    //generateEtags: false,
+    reactStrictMode: false,
+    analyticsId: '7vsHWbYwcKpA3lFTng2Qe9iNzE7',
+    env: {
+      siteTitle: "IGL DAPP",
+      contractAddress: "0xC81e9A17a5BAEE1141BA2AC7EEFDbe8f450b60e8",
+      infuraID: "045e9c65387a4200aa8559c347fa19a4",
+      defaultGas: 125000,//75000,
+      mintType: "Pre-Sale", //Pre-Sale Public
+      enableBogo: true,
+      enforceWhitelist: true,
+      ethValue: "0.44",
+      ethWLValue: "0.0",
+      debug: false,
+      network: 'mainnet',
+      chainId: 1,
+      //mintType: "Whitelist",
+      blockExplorerURL: 'https://etherscan.io/',
+      www: 'https://www.iglnft.app',
+      mainWWW: 'https://www.iglnft.app',
+      title: 'IGL NFT',
+      links: {
+        opensea: "https://opensea.io/collection/igl-genesis-nft",
+        discord: "https://discord.gg/Egw5QN9hZQ",
+        twitter: "https://twitter.com/iglnft"
+      },
+      maxMint: 8000,
+      maxMintCount: 50,
+      messagehash: '0x865f293da4a629f00bd8072be6115031c608fa4a13b819fd087811752fc8bb90',
+      signatureBytes: '0x4b7715f34823936b045d1e5ca6c295b7f5ecff3d0c158118c71441ed217c3ddb026617ce49dc15781bdb25c8c34ed4a1f4d68df6080f6dd0004511cf05032bf01c',
+      messagehash2: '0x417ebaa8de17c9a73acf7cf3f6c998ca2dd1e993276cc9dd37dfb7fb0cb197a4',
+      signatureBytes2: '0xe191166afd511a08818c2f55906e65db02dfe2366708885e92a2e54cef7ac2673e5dfe70c2d0d63a337c7792906a623f3aff0506aa2c5de6325e2d689bab95731b'
+    },
+    images: {
+      domains: ['static.wixstatic.com']
+    }
+  }
